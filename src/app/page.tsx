8a9c42a48ac0d8d2c0f94cff7daa24c8b6b4e7bb
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-12-15 08:36:47 -0500

    🪺 (10:17)Install Button component from shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <h1 className="text-red-600">hello world</h1>
+    <Button>Hello World</Button>
   )
 }
