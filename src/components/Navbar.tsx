4d52182511835f2f0493772ba641397090fe064d
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-12-16 11:08:43 -0500

    🦜 (43:34) Prompt the user name data when is logged in

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,29 +6,28 @@ type Props = {}
 
 const Navbar = async (props: Props) => {
   const session = await getAuthSession();
-  if (session?.user) {
-    return (
-      <pre>{JSON.stringify(session.user, null, 2)}</pre>
-    );
-  } else {
-    return (
-      <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
-        <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
 
-          <Link href="/" className="flex items-center gap-2">
-            <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white">
-              Quizzy
-            </p>
-          </Link>
+  return (
+    <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
+      <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
 
-          <div className="flex items-center">
+        <Link href="/" className="flex items-center gap-2">
+          <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white">
+            Quizzy
+          </p>
+        </Link>
+
+        <div className="flex items-center">
+          {session?.user ? (
+            <h1>Welcome {session?.user?.name}</h1>
+          ) : (
             <SignInButton text="Sign In" />
-          </div>
-
+          )}
         </div>
+
       </div>
-    );
-  };
+    </div>
+  );
 }
 
 export default Navbar
