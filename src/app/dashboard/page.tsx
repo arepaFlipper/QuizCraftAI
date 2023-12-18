1fcbc85568aae03ec4fff6c3f4002f94100aa134
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-12-18 07:50:41 -0500

    🐑 (1:08:55) Dev Dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,29 @@
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
+
 type Props = {}
 
-const Dashboard = (props: Props) => {
+export const metadata = {
+  title: "Dashboard | Quiz Craft AI"
+}
+
+const Dashboard = async ({ }: Props) => {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    return redirect("/");
+  }
   return (
-    <div>Dashboard</div>
+    <main className="p-8 mx-auto max-w-7xl">
+      <div className="flex items-center">
+        <h2 className="mr-2 text-3xl font-bold tracking-tight">Dashboard</h2>
+      </div>
+
+      <div className="grid gap-4 mt-4 md:grid-cols-2">
+        {/* <QuizMeCard /> */}
+        {/* <HistoryCard /> */}
+      </div>
+      <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7"> </div>
+    </main>
   )
 }
 
