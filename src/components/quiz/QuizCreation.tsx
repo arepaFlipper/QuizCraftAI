dddff7466ce7917c31e24f7f84d6dd13a5bfc2f5
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-12-19 09:37:49 -0500

    🥑 (1:31:22) add zod library to validate forms:
    There is a skip on tutorial at this point

--- src/components/quiz/QuizCreation.tsx
+++ src/components/quiz/QuizCreation.tsx
@@ -1,7 +1,22 @@
 import { Card, CardDescription, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { useForm } from "react-hook-form";
+import { quizCreationSchema } from "@/schemas/form/quiz";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 type Props = {}
 
+type Input = z.infer<typeof quizCreationSchema>;
+
 const QuizCreation = ({ }: Props) => {
+  const form = useForm<Input>({
+    resolver: zodResolver(quizCreationSchema),
+    defaultValues: {
+      amount: 3,
+      topic: "",
+      type: "open_ended",
+    }
+  });
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <Card>
