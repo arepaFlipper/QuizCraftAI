3dce49a25aa32e7774d7005bd7aa5dec81305889
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-12-20 04:47:55 -0500

    🙌 (2:01:03) use the KEY to generate questions & answers

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,4 +1,7 @@
 import { NextResponse } from "next/server";
+import { quizCreationSchema } from "@/schemas/form/quiz"
+import { ZodError } from "zod";
+import { strict_output } from "@/lib/gpt";
 
 // NOTE: GET http://localhost:3000/api/questions
 export const GET = async (req: Request, res: Response) => {
@@ -7,5 +10,30 @@ export const GET = async (req: Request, res: Response) => {
 
 // NOTE: POST http://localhost:3000/api/questions
 export const POST = async (req: Request, res: Response) => {
-  return NextResponse.json({ hello: `world This is a ${req.method} request` });
+  try {
+    const body = await req.json();
+    const { amount, topic, type } = quizCreationSchema.parse(body);
+    let questions: any;
+    let message1 = "Your are a helpful AI that is able to generato a pair of questions and answers. "
+    message1 += "The length of the answer should not exceed 15 words."
+    message1 += "Store all the pairs of answers and questions in a JSON array."
+    if (type === "open_ended") {
+      let message2 = `Please generate a random hard open-ended questions about ${topic}`
+
+      const output_format = { question: "question", answer: "answer with max length of 15 words" };
+
+      questions = await strict_output(message1, message2, output_format);
+    } else if (type === "mcq") {
+      let message2 = `Please generate a random hard multiple choice questions about ${topic}`
+
+      const output_format = { question: "question", answer: "answer with max length of 15 words" };
+      questions = await strict_output(message1, message2, output_format);
+    }
+    return NextResponse.json({ questions, status: 200 });
+  } catch (error: any) {
+    console.error(error);
+    if (error instanceof ZodError) {
+      return NextResponse.json({ error: error.issues }, { status: 400 });
+    }
+  }
 }
