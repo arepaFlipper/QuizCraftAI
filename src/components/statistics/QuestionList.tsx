4c61946d1f9470a393ab40cff057fccf24cbd7dc
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-12-23 10:08:41 -0500

    😁 (4:05:09) Display Statistics cards:Accuracy,Results,TimeTaken

---
+++ src/components/statistics/QuestionList.tsx
@@ -0,0 +1,13 @@
+import { Question } from "@prisma/client";
+
+type Props = {
+  questions: Question[];
+}
+
+const QuestionList = (props: Props) => {
+  return (
+    <div>QuestionList</div>
+  )
+}
+
+export default QuestionList
