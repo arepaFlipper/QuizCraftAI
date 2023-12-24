c9b3e9d3142ab2ad1dd055cb23483adde44d1a62
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-12-24 10:55:38 -0500

    🙀 (4:36:03) Quiz History on main page

--- src/components/dashboard/RecentActivities.tsx
+++ src/components/dashboard/RecentActivities.tsx
@@ -1,16 +1,24 @@
 import { Card, CardDescription, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { getAuthSession } from "@/lib/nextauth";
+import HistoryComponent from "@/components/HistoryComponent";
+import { redirect } from "next/navigation";
 
 type Props = {}
 
-const RecentActivities = ({ }: Props) => {
+const RecentActivities = async ({ }: Props) => {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    return redirect("/");
+  }
+
   return (
     <Card className="col-span-4 lg:col-span-3">
       <CardHeader>
         <CardTitle className="text-2xl font-bold">Recent Activities</CardTitle>
         <CardDescription>You have played a total of 7 games</CardDescription>
       </CardHeader>
-      <CardContent className="max-h-[580px] overlow-scroll">
-        Histories
+      <CardContent className="max-h-[580px] overflow-scroll">
+        <HistoryComponent limit={10} userId={session.user.id} />
       </CardContent>
     </Card>
   )
