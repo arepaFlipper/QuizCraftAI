af730fe8a71110e2ef71780d5019bd0f504a4bc0
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-12-24 11:00:18 -0500

    🕉️ (4:36:47) fetch the games_count but there is a BUG

--- src/components/dashboard/RecentActivities.tsx
+++ src/components/dashboard/RecentActivities.tsx
@@ -2,6 +2,7 @@ import { Card, CardDescription, CardHeader, CardTitle, CardContent } from "@/com
 import { getAuthSession } from "@/lib/nextauth";
 import HistoryComponent from "@/components/HistoryComponent";
 import { redirect } from "next/navigation";
+import { prisma } from "@/lib/db";
 
 type Props = {}
 
@@ -11,11 +12,15 @@ const RecentActivities = async ({ }: Props) => {
     return redirect("/");
   }
 
+  const games_count = await prisma.game.count({
+    where: { userId: session.user.id }
+  })
+
   return (
     <Card className="col-span-4 lg:col-span-3">
       <CardHeader>
         <CardTitle className="text-2xl font-bold">Recent Activities</CardTitle>
-        <CardDescription>You have played a total of 7 games</CardDescription>
+        <CardDescription>You have played a total of {games_count} games</CardDescription>
       </CardHeader>
       <CardContent className="max-h-[580px] overflow-scroll">
         <HistoryComponent limit={10} userId={session.user.id} />
